291fcf32779d099fd9875e9b2fd601774061e9f2
lonac <barakamalonao@yahoo.com>
2017-05-05 15:39:35 +0300

    app/Http/Controllers/RoleStaffController.php database/migrations/2017_05_05_123757_RoleStaffTable.php

--- app/Http/Controllers/RoleStaffController.php
+++ app/Http/Controllers/RoleStaffController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class RoleStaffTable extends Controller
+class RoleStaffController extends Controller
 {
     /**
      * Display a listing of the resource.
